f6e5bfe2c3a8a54b75dddb1d6da437f068fb9154
jillybean2006 <kjillian2025@gmail.com>
2025-12-10 19:35:53 -0600

    Fix various issues in app.js for exercises

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const foods = [];  
 
 console.log('Exercise 1 result:', foods);
-foods = [];
 foods.push("pizza");
 foods.push("cheeseburger");
 
@@ -15,7 +14,7 @@ console.log('Exercise 3 result:', foods);
 const favFood = foods[1];
 console.log('Exercise 4 result:', favFood);
 
-foods.splice(1,1, "tofu");
+foods.splice(1,0, "tofu");
 console.log('Exercise 5 result:', foods);
 
 foods.splice(foods.indexOf("pizza"), 1, "sushi", "cupcake");
@@ -34,19 +33,19 @@ console.log('Exercise 9 result:', allFoods);
 let hasSoup = foods.includes("soup");
 console.log('Exercise 10 result:', hasSoup);
 
-let odds = [];
-num.forEach.(function(num) {
-  if (num % 2 !== 0) {
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]; 
+let odds = []; nums.forEach((num) => { 
+  if (nums  % 2!== 0) {
     odds.push(num);
   }
 });
 console.log('Exercise 11 result:', odds);
 
 
-const frizz = [];
-const buzz [];
+const fizz = [];
+const buzz = [];
 const fizzBuzz = [];
-num.forEach.(function(num) {
+num.forEach(function(num) {
     if (num % 3 === 0) {
         frizz.push(num);
         } 
@@ -75,10 +74,9 @@ const num = numArrays[2][1];
 console.log('Exercise 14 result:', num);
 
 
-let total = 0;
-numArrays.forEach(innerArray => {
-    innerArray.forEach(number => {
-        total += num;
+let total = 0; 
+numArrays.forEach(innerArray => { innerArray.forEach(number => 
+  { total += number;
     });
 
 });
